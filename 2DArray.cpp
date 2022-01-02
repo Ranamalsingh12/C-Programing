b3e9b25c26bd9cba7953fd105dc8a2414bc0605d
ranamalsingh12 <ranams99911@gmail.com>
2022-01-02 17:07:58 +0530

    commit

---
+++ 2DArray.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    int n,ml
+    cin>>n,m;
+    int arr[n],[m];
+    cout<<"Printing a 2D Array:\n";
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<2;j++)
+        {
+            cout<<"\t"<<arr[i][j];
+        }
+        cout<<endl;
+    }
+
+ 
+    // int n,m;
+    // cin>>n,m;
+
+    // // initialising the 2D array
+    // int arr[n][m];
+    
+    // //input and output of 2D array
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<m;j++){
+    //         cin>>arr[i][j];
+    //     }
+    // }
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<m;j++){
+    //         cout<<arr[i][j]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    return 0;
+}
