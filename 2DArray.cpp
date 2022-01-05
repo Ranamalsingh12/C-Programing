3e7960c55387d6311fe732bd882fd1b65e431d4c
ranamalsingh12 <ranams99911@gmail.com>
2022-01-06 01:40:15 +0530

    commit

--- 2DArray.cpp
+++ 2DArray.cpp
@@ -7,16 +7,6 @@ int main(){
     int n,ml
     cin>>n,m;
     int arr[n],[m];
-    cout<<"Printing a 2D Array:\n";
-    for(int i=0;i<4;i++)
-    {
-        for(int j=0;j<2;j++)
-        {
-            cout<<"\t"<<arr[i][j];
-        }
-        cout<<endl;
-    }
-
  
     // int n,m;
     // cin>>n,m;
