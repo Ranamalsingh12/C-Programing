4cf49ef22a54e9938f532490c989a03a1a6e126f
ranamalsingh12 <ranams99911@gmail.com>
2022-01-06 23:17:51 +0530

    commit

--- 2DArray.cpp
+++ 2DArray.cpp
@@ -4,27 +4,25 @@ using namespace std;
 
 int main(){
 
-    int n,ml
+    int n,m;
     cin>>n,m;
-    int arr[n],[m];
- 
-    // int n,m;
-    // cin>>n,m;
 
-    // // initialising the 2D array
-    // int arr[n][m];
+    // initialising the 2D array
+    int arr[n][m];
     
-    // //input and output of 2D array
-    // for(int i=0;i<n;i++){
-    //     for(int j=0;j<m;j++){
-    //         cin>>arr[i][j];
-    //     }
-    // }
-    // for(int i=0;i<n;i++){
-    //     for(int j=0;j<m;j++){
-    //         cout<<arr[i][j]<<" ";
-    //     }
-    //     cout<<endl;
-    // }
+    //input and output of 2D array
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    cout<<"Matrix is"<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
