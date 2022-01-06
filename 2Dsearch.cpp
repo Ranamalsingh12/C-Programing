44ccea3478c9c414d140ae65e9c86275d4c203d2
ranamalsingh12 <ranams99911@gmail.com>
2022-01-06 23:44:29 +0530

    commit

---
+++ 2Dsearch.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    int n,m;
+    cin>>n;
+    cin>>m;
+
+    int arr[n][m];
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    int x;
+    cout<<"Enter the number which you want: ";
+    cin>>x;
+    bool flag = false;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(arr[i][j]==x){
+                cout<<i<<" "<<j<<endl;
+                flag = true;
+            }
+        }
+        cout<<endl;
+    }
+    if (flag){
+        cout<<"element is found"<<endl;
+    }
+    return 0;
+}
