7af0bd7bf4b0825536e99acf0e64f86d1e290d86
ranamalsingh12 <ranams99911@gmail.com>
2022-01-21 14:12:49 +0530

    commit

---
+++ intersection.cpp
@@ -0,0 +1,16 @@
+vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
+{
+    // vector<int> ans;
+	// for (int i=0;i<n;i++){
+    //     int element = arr1[i];
+        
+    //     for(int j=0;i<m;j++){
+    //         if(element == arr2[j]){
+    //             ans.push_back(element);
+    //             arr2[j] = -112231;
+    //             break;
+    //         }
+    //     }
+    // }
+    // return ans;
+}
