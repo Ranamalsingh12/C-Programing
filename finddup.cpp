39a33812093b7dcff11b9f98eb1901ccd8d54965
ranamalsingh12 <ranams99911@gmail.com>
2022-01-21 13:42:25 +0530

    commit

--- finddup.cpp
+++ finddup.cpp
@@ -1,14 +1,20 @@
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
 
-int main(){
-    int n;
-    cin>>n;
-
-    int arr[n];
-    for (int i=0;i<n;i++){
-        cin>>arr[i];
-    }
+// int main()
+int findDuplicate(vector<int> &arr)
+{
+    int ans=0;
     
+    for (int i=0; i<arr.size();i++)
+    {
+        ans = ans^arr[i];
+    }
+
+    for (int i=1; i<arr.size();i++)
+    {
+        ans = ans^i;
+    }
+    return ans;
 }
