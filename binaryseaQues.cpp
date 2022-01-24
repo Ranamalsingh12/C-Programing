92a69652e7d9e1c2cac4baa4a820911f0ff19384
ranamalsingh12 <ranams99911@gmail.com>
2022-01-24 18:32:24 +0530

    peak mountain problem

--- binaryseaQues.cpp
+++ binaryseaQues.cpp
@@ -67,5 +67,24 @@ int main(){
     cout<<"Total no of occurence of 3 are "<< totalOcc << endl;
 
     //peak index in the mountain
-    
+    class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int s = 0;
+        int e = arr.size()-1;
+        
+        int mid = s + (e-s)/2;
+        
+        while(s<e){
+            if(arr[mid]<arr[mid+1]){
+                s = mid +1;
+            }
+            else{
+                e = mid;
+            }
+            mid = s + (e-s)/2;
+        }
+        return s;
+    }
+};
 }
